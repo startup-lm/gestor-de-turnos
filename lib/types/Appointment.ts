037aef997e29afec025f19956765f0d5bda36f35
mauriciooubina <mauriciooubina@gmail.com>
2025-07-06 22:14:20 -0300

    add lib

---
+++ lib/types/Appointment.ts
@@ -0,0 +1,19 @@
+export interface Appointment {
+    id: number;
+    client: string;
+    barber_id: number;
+    date: string;
+    start_time: string;
+    end_time: string;
+    phone: string;
+    paid: boolean;
+    price: number;
+    service_id: number;
+    barbers?: {
+      name?: string;
+    } | null;
+    services?: {
+      name?: string;
+    } | null;
+  }
+  
---
+++ lib/types/CalendarEvent.ts
@@ -0,0 +1,7 @@
+export type CalendarEvent = {
+    id: number;
+    title: string;
+    start: Date;
+    end: Date;
+    paid: boolean;
+};
